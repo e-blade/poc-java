3d6fdc1d38ded5ffe035de3d95cb20e2f0b65469
Vinicius Serpa <vinicius.serpa@gmail.com>
2015-07-16 12:50:41 -0300

    Create class StringEncoder to generate hash using SpringSecurity

---
+++ Foundation/src/main/java/br/com/eblade/poc/foundation/Run.java
@@ -0,0 +1,13 @@
+package br.com.eblade.poc.foundation;
+
+import br.com.eblade.poc.foundation.crypto.StringEncoder;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		System.out.println(StringEncoder.encode("vinicius"));
+
+	}
+
+}
---
+++ Foundation/src/main/java/br/com/eblade/poc/foundation/crypto/StringEncoder.java
@@ -0,0 +1,28 @@
+package br.com.eblade.poc.foundation.crypto;
+
+import org.springframework.security.authentication.encoding.MessageDigestPasswordEncoder;
+
+public abstract class StringEncoder {
+
+	// salt is used with null value
+	private static Object salt;
+	private static String algorithm = "MD5";
+	
+	public static String encode(String pass) {		
+		MessageDigestPasswordEncoder encoder = getInstance();		
+		String encodedPass = encoder.encodePassword(pass, salt);		
+		return encodedPass;		
+	}
+	
+	public static boolean isValid(String pass, String encodedPass) {		
+		MessageDigestPasswordEncoder encoder = getInstance();
+		return encoder.isPasswordValid(encodedPass, pass, salt);
+	}
+	
+	// Factory
+	private static MessageDigestPasswordEncoder getInstance() {		
+		MessageDigestPasswordEncoder encoder = new MessageDigestPasswordEncoder(algorithm);		
+		return encoder;
+	}	
+		
+}
